d88b10b4703e4b09ab743c16f74e6a1e9769377a
yamao-sys <yamaopanku@gmail.com>
2025-01-13 17:50:30 +0900

    Playwrightの追加

---
+++ run-e2e-local.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# テスト実行
+docker-compose -f docker-compose.e2e.yaml run --rm frontend_test npm run e2e
+
+# テスト用DBのリセット
+docker-compose -f docker-compose.e2e.yaml run --rm db mysql -h db -u root -p -e "DROP DATABASE react_router_v7_practice_test; CREATE DATABASE react_router_v7_practice_test;"
+docker-compose -f docker-compose.e2e.yaml run --rm api_server_test sh -c 'cd db && godotenv -f /app/.env.test.local sql-migrate up -env="mysql"'
