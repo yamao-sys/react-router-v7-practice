73f1264ad55b10d6982c62cc432aded6d603362f
yamao-sys <yamaopanku@gmail.com>
2025-01-13 16:45:35 +0900

    ESLintの適用範囲からreact-routerディレクトリを除外

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -1,37 +1,37 @@
-import globals from 'globals';
-import reactHooks from 'eslint-plugin-react-hooks';
-import reactRefresh from 'eslint-plugin-react-refresh';
-import reactPlugin from 'eslint-plugin-react';
-import importPlugin from 'eslint-plugin-import';
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
-import { FlatCompat } from '@eslint/eslintrc';
-import eslintConfigPrettier from 'eslint-config-prettier';
-import * as fs from 'fs';
-import typescriptParser from '@typescript-eslint/parser';
-import typescriptPlugin from '@typescript-eslint/eslint-plugin';
+import globals from "globals"
+import reactHooks from "eslint-plugin-react-hooks"
+import reactRefresh from "eslint-plugin-react-refresh"
+import reactPlugin from "eslint-plugin-react"
+import importPlugin from "eslint-plugin-import"
+import { dirname } from "path"
+import { fileURLToPath } from "url"
+import { FlatCompat } from "@eslint/eslintrc"
+import eslintConfigPrettier from "eslint-config-prettier"
+import * as fs from "fs"
+import typescriptParser from "@typescript-eslint/parser"
+import typescriptPlugin from "@typescript-eslint/eslint-plugin"
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
-});
+})
 
-const domains = fs.readdirSync('./app');
+const domains = fs.readdirSync("./app")
 const zones = domains.map((domain) => ({
   from: `./app/${domain}/!(public)/**/*`,
   target: `./app/!(${domain})/**/*`,
-}));
+}))
 
 const eslintConfig = [
-  ...compat.extends('plugin:prettier/recommended'),
+  ...compat.extends("plugin:prettier/recommended"),
   reactPlugin.configs.flat.recommended,
-  reactPlugin.configs.flat['jsx-runtime'],
+  reactPlugin.configs.flat["jsx-runtime"],
   {
     settings: {
       react: {
-        version: 'detect',
+        version: "detect",
       },
     },
   },
@@ -42,25 +42,25 @@ const eslintConfig = [
       ...eslintConfigPrettier.rules,
 
       // NOTE: app配下はそれぞれが専属で使用するため、他のドメインのディレクトリからimport不可にする
-      'import/no-restricted-paths': [
-        'error',
+      "import/no-restricted-paths": [
+        "error",
         {
           zones,
         },
       ],
-      'react/jsx-uses-react': 'error',
-      'react/jsx-uses-vars': 'error',
-      '@typescript-eslint/no-unused-vars': 'warn',
+      "react/jsx-uses-react": "error",
+      "react/jsx-uses-vars": "error",
+      "@typescript-eslint/no-unused-vars": "warn",
     },
   },
-  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
+  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
   {
     languageOptions: {
       parser: typescriptParser,
       parserOptions: {
         ecmaVersion: 2022,
-        sourceType: 'module',
-        project: './tsconfig.json',
+        sourceType: "module",
+        project: "./tsconfig.json",
         tsconfigRootDir: __dirname,
       },
       globals: globals.browser,
@@ -68,13 +68,13 @@ const eslintConfig = [
   },
   {
     plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
       import: importPlugin,
-      '@typescript-eslint': typescriptPlugin,
+      "@typescript-eslint": typescriptPlugin,
     },
   },
-  { ignores: ['app/**/__generated__/**/*.ts'] },
-];
+  { ignores: ["app/**/__generated__/**/*.ts", ".react-router/**/*.ts"] },
+]
 
-export default eslintConfig;
+export default eslintConfig
