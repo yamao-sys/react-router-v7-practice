21f6f2c9fbf1ea5200ef17745a7304b10a106f77
yamao-sys <yamaopanku@gmail.com>
2025-01-18 14:02:53 +0900

    CIのE2Eのフロントエンドのデバッグコード

--- frontend/playwright.config.ts
+++ frontend/playwright.config.ts
@@ -10,6 +10,7 @@ import { fileURLToPath } from "url"
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
 const envFilePath = process.env.CI ? ".env.test" : ".env.test.local"
+console.log(envFilePath)
 dotenv.config({ path: path.resolve(__dirname, envFilePath) })
 
 /**
