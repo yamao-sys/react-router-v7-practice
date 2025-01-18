bb7f31af20ef4cd55f2761c4a03b82a6ea4d43ef
yamao-sys <yamaopanku@gmail.com>
2025-01-18 14:27:17 +0900

    ログインページのE2E追加

---
+++ frontend/e2e/sign_in.spec.ts
@@ -0,0 +1,22 @@
+import { test, expect } from "@playwright/test"
+
+test("SignIn Successfully", async ({ page }) => {
+  await page.goto("/sign_up")
+
+  // NOTE: 会員登録フォームを入力
+  await page.getByRole("textbox", { name: "ユーザ名" }).fill("test_login")
+  await page.getByRole("textbox", { name: "Email" }).fill("test_login@example.com")
+  await page.getByRole("textbox", { name: "パスワード" }).fill("password")
+
+  await page.getByRole("button", { name: "登録する" }).click()
+
+  // NOTE: ログイン
+  await page.goto("/sign_in")
+  await page.getByRole("textbox", { name: "Email" }).fill("test_login@example.com")
+  await page.getByRole("textbox", { name: "パスワード" }).fill("password")
+
+  await page.getByRole("button", { name: "ログインする" }).click()
+
+  await page.waitForURL("/")
+  await expect(page).toHaveURL("/")
+})
