b875b14bf26ed7e27de08e818c5ef93dd7c49207
yamao-sys <yamaopanku@gmail.com>
2025-01-18 13:25:34 +0900

    会員登録のE2Eを調整

--- frontend/e2e/sign_up.spec.ts
+++ frontend/e2e/sign_up.spec.ts
@@ -10,8 +10,9 @@ test("SignUp Successfully", async ({ page }) => {
 
   await page.getByRole("button", { name: "登録する" }).click()
 
-  // NOTE: 登録に成功すると、フォーム内容がクリアされる
-  await expect(page.getByRole("textbox", { name: "ユーザ名" })).toHaveValue("")
+  // NOTE: 登録に成功すると、ログイン画面に遷移する
+  await page.waitForURL("/sign_in")
+  await expect(page.getByRole("heading", { name: "ログインフォーム" })).toBeVisible()
 })
 
 test("SignUp ValidationError", async ({ page }) => {
@@ -19,7 +20,7 @@ test("SignUp ValidationError", async ({ page }) => {
 
   await page.getByRole("button", { name: "登録する" }).click()
 
-  await expect(page.getByRole("textbox", { name: "ユーザ名は必須入力です。" })).toBeVisible()
-  await expect(page.getByRole("textbox", { name: "Emailは必須入力です。" })).toBeVisible()
-  await expect(page.getByRole("textbox", { name: "パスワードは必須入力です。" })).toBeVisible()
+  await expect(page.getByText("ユーザ名は必須入力です。")).toBeVisible()
+  await expect(page.getByText("Emailは必須入力です。")).toBeVisible()
+  await expect(page.getByText("パスワードは必須入力です。")).toBeVisible()
 })
