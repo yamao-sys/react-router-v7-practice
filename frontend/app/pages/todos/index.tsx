f2a6d12585de35cc94704d80ba222318035a1eb3
yamao-sys <yamaopanku@gmail.com>
2025-01-19 22:23:35 +0900

    TODOリストの削除

--- frontend/app/pages/todos/index.tsx
+++ frontend/app/pages/todos/index.tsx
@@ -3,9 +3,15 @@ import { TodosList } from "./components/TodosList"
 import { Route } from "./+types"
 import { graphQLClient } from "@/lib/graphQLClient"
 import { FetchTodoListsDocument, FetchTodoListsQuery } from "./__generated__"
-import { useLoaderData } from "react-router"
+import { useFetcher, useLoaderData } from "react-router"
 import { getAuthHeader } from "@/lib/authCookie"
 import { TodosLayout } from "./layout"
+import {
+  DeleteTodoDocument,
+  DeleteTodoMutation,
+  TodosList_TodoFragment,
+} from "./components/TodosList/__generated__"
+import { useEffect, useState } from "react"
 
 gql`
   query fetchTodoLists {
@@ -25,13 +31,43 @@ export async function loader({ request }: Route.LoaderArgs) {
   return { todos: data.fetchTodoLists }
 }
 
+export async function action({ request }: Route.ActionArgs) {
+  const formData = await request.formData()
+  const id = formData.get("id")
+
+  const cookie = await getAuthHeader(request)
+  const { data } = await graphQLClient(cookie).mutate<DeleteTodoMutation>({
+    mutation: DeleteTodoDocument,
+    variables: {
+      id,
+    },
+  })
+
+  console.log(`data?.deleteTodo: ${data?.deleteTodo}`)
+
+  return { todoId: data?.deleteTodo, success: !!data?.deleteTodo }
+}
+
 export default function TodosPage() {
   const { todos } = useLoaderData<typeof loader>()
+  const [displayableTodos, setDisplayableTodos] = useState<Array<TodosList_TodoFragment>>(todos)
+
+  const fetcher = useFetcher<{ todoId: string; success: boolean }>()
+
+  useEffect(() => {
+    if (!fetcher.data?.success) return
+
+    const todoId = fetcher.data?.todoId
+    const todo = displayableTodos.find((t) => Number(t.id) === Number(todoId))
+    window.alert(`${todo?.title}の削除に成功しました!`)
+
+    setDisplayableTodos((prev) => prev.filter((t) => Number(t.id) !== Number(todoId)))
+  }, [fetcher.data])
 
   return (
     <TodosLayout>
       <div className='mt-16'>
-        <TodosList todos={todos} />
+        <TodosList todos={displayableTodos} fetcher={fetcher} />
       </div>
     </TodosLayout>
   )
